c362fee4f385e6488e4848444ff5a19b4e5dc46b
denysbuedo <45684658+denysbuedo@users.noreply.github.com>
2019-02-22 23:48:53 +0800

    Update workspace path

--- jenkins.sh
+++ jenkins.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 if [[ $1 = 'run' ]];
   then
-      /usr/local/MATLAB/R2018a/bin/matlab -nodisplay < /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/Main_jRun.m
+      /usr/local/MATLAB/R2018a/bin/matlab -nodisplay < /root/matlab/$7/Main_jRun.m
   elif [[ $1 = 'test' ]];
     then
       /usr/local/MATLAB/R2018a/bin/matlab -nodisplay < /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/Main_jTest.m
   elif [[ $1 = 'delivery' ]];
     then
-      tar fcz /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/$7.tar.gz --absolute-names /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/results/
-      rm -f /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/data/run/$3
-      rm -f /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/data/run/$4
-      rm -f /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/data/run/$5
-      rm -f /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/data/run/$6
-      rm -f /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/data/run/data.txt
+      tar fcz /root/matlab/$7/$7.tar.gz --absolute-names /root/matlab/$7/results/
       if [ -d "/media/DATA/FTP/Matlab/BC-Vareta/$2" ]
        then
-	   mv /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/$7.tar.gz /media/DATA/FTP/Matlab/BC-Vareta/$2
+	  	   mv /root/matlab/$7/$7.tar.gz /media/DATA/FTP/Matlab/BC-Vareta/$2
+	   	   rm -rf /root/matlab/$7
        else
            mkdir /media/DATA/FTP/Matlab/BC-Vareta/$2
-           mv /root/matlab/BC-VARETA-toolbox-master/BC-VARETA-toolbox-master/$7.tar.gz /media/DATA/FTP/Matlab/BC-Vareta/$2
+           mv /root/matlab/$7/$7.tar.gz /media/DATA/FTP/Matlab/BC-Vareta/$2
+           rm -rf /root/matlab/$7 
        fi
     else
       echo "Invalid action"
